f001db155f0c882adb91de81b02c2c8fef645bea
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-04 11:56:00 -0500

    setup express server with proof of life route

---
+++ app.js
@@ -0,0 +1,29 @@
+/** @format */
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const handle404Error = require('./src/middleware/404');
+const handle500Error = require('./src/middleware/500');
+const router = require('./src/routes/routes');
+
+// Enable Cors on all routes
+app.use(cors());
+
+// Parse JSON request bodies
+app.use(express.json());
+
+// Routes
+app.use(router);
+
+// 404 Error Handler
+app.use(handle404Error);
+
+// 500 Error Handler
+app.use(handle500Error);
+
+// Start the server
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
