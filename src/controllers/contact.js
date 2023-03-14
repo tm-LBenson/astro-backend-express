dfe46b92ae9fe4dc67b7558e285c957549735339
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-14 14:27:17 -0400

    implement email on contact route

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,9 +1,33 @@
 /** @format */
 
+const nodemailer = require('nodemailer');
+
 function contact(req, res) {
-  console.log(req.body);
-  res.status(200).send({
-    connected: 'connected',
+  const { name, email, message } = req.body;
+  const EMAIL = process.env.EMAIL;
+  const PASS = process.env.PASS;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: EMAIL,
+      pass: PASS,
+    },
+  });
+  const mailOptions = {
+    from: email,
+    to: EMAIL,
+    subject: `New message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log(`Email sent: ${info.response}`);
+      res.status(200).send({ status: 'Message sent' });
+    }
   });
 }
 
